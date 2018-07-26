737718c5bcea5222c246e241536557446a7aca3d
ivan-moskvin <i.moskvin@modulbank.ru>
2018-07-26 22:40:08 +0700

    Added flickr cat search (thx MPJ!) Added shortid

---
+++ examples/playground/src/catSearch.js
@@ -0,0 +1,18 @@
+export default function getCats (keyword, page = 1) {
+    const apiKey = '9df9f716c535e20573c4d5fc7336e3c6'
+    return fetch(
+      'https://api.flickr.com/services/rest/' +
+      '?method=flickr.photos.search' +
+      '&api_key=' + apiKey +
+      '&page=' + page +
+      '&tags=' + keyword +
+      '&format=json' +
+      '&nojsoncallback=1'
+    )
+    .then(response => response.json())
+    .then(body => body.photos.photo)
+    .then(photos => photos.map(photo =>
+      `https://farm${photo.farm}.staticflickr.com/` +
+      `${photo.server}/${photo.id}_${photo.secret}_q.jpg`
+    ))
+  }
