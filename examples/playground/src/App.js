02fb4bb4c3d1777f4afbd7cd7d4cf218665c3ad3
ivan-moskvin <i.moskvin@modulbank.ru>
2018-09-23 12:24:44 +0700

    Added cat examples

--- examples/playground/src/App.js
+++ examples/playground/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-// import fetchCats from './catSearch';
+import fetchCats from './catSearch';
+import Cat from './components/cat';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    cats : []
+  };
+
+  constructor() {
+    super();
+    fetchCats('cute cat', 1).then(cats => {
+      this.setState({cats: cats})
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'} className="App-logo" alt="logo" />
           <h1 className="App-title">Добро пожаловать</h1>
         </header>
+        {this.state.cats.map(cat => <Cat image={cat}/>)}
+        <Cat image={'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'}/>
         <p className="App-intro">
-          Йоу
+          Йоу, братва
         </p>
       </div>
     );
---
+++ examples/playground/src/components/cat.js
@@ -0,0 +1,7 @@
+
+
+import React from 'react';
+
+const Cat = ({image}) => <img src={image} />;
+
+export default Cat;
